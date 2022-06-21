9b50d2eff9e0c73c483cc94c7f9b2f38bc63e439
Hamza Kermadi <hakermad@fu-r1-p7.s19.be>
2022-06-21 18:17:49 +0200

    :sparkles: cd works with syntax_error

--- src/syntax_error.c
+++ src/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: hakermad <hakermad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 17:13:54 by lrondia           #+#    #+#             */
-/*   Updated: 2022/06/21 17:20:53 by hakermad         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:17:19 by hakermad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	is_special_char(char c)
 {
 	return (!ft_isalnum(c) && !is_metachar(c) && c != '_'
 		&& c != ' ' && c != '\\' && c != '$' && c != '"' && c != '\''
-		&& c != '=');
+		&& c != '=' && c != '.' && c != '/');
 }
 
 void	check_wrong_seperators(t_data *data, char *line)
