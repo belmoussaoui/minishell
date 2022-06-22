37695280da9ce4bb5b09fad5973829934d298ddf
Louise Rondia <louiserondia@gmail.com>
2022-06-22 13:53:39 +0200

    :bug: Fixed split problem

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: lrondia <lrondia@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:34:39 by lrondia           #+#    #+#             */
-/*   Updated: 2022/06/22 13:44:43 by lrondia          ###   ########.fr       */
+/*   Updated: 2022/06/22 13:53:14 by lrondia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ void	lexer(t_data *data, t_list **commands, char *line)
 	cmd->elements = ft_split_quote(data, tmp, ' ');
 	if (!cmd->elements)
 		exit(EXIT_FAILURE);
-	// debug_split(cmd->elements);
 	fd_pipe(data, cmd);
 	new = ft_lstnew(cmd);
 	if (!new)
